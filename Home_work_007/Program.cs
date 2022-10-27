9ea939e39ae55a25055c277b3f1370e713c4a7e1
Klin <piter_frst@yahoo.com>
2022-10-27 21:57:23 +0300

    Task 3 added

--- Home_work_007/Program.cs
+++ Home_work_007/Program.cs
@@ -9,7 +9,7 @@ while (begin)
     Console.WriteLine("Введите номер интересующей вас программы:");
     Console.WriteLine("1 - Программа, создаёт двумерный массив размером m х n, заполненный случайными вещественными числами.");
     Console.WriteLine("2 - Программа, принимает индекс элемента в двумерном массиве, и возвращает значение этого элемента или же сообщит, что такого элемента нет.");
-    Console.WriteLine("3 - Программа, .");
+    Console.WriteLine("3 - Программа, задаёт двумерный массив из целых чисел и находит среднее арифметическое элементов в каждом столбце.");
     Console.WriteLine("4 - Если хотите покинуть программу.");
     system = Convert.ToInt32(Console.ReadLine());
 
@@ -149,17 +149,80 @@ while (begin)
 
         case 3:
             /*                                              ///Ветка task_3.
-            Задача 47. Задайте двумерный массив размером m×n, 
-            заполненный случайными вещественными числами.
-            m = 3, n = 4.
-            0,5 7 -2 -0,2
-            1 -3,3 8 -9,9
-            8 7,8 -7,1 9
+            Задача 52. Задайте двумерный массив из целых чисел. 
+            Найдите среднее арифметическое элементов в каждом столбце.
+            Например, задан массив:
+            1 4 7 2
+            5 9 2 3
+            8 4 2 4
+            Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
             */
             Console.Clear();
-            Console.WriteLine("ПРОГРАММА 1 ЗАПУЩЕНА\n");
+            Console.WriteLine("ПРОГРАММА 3 ЗАПУЩЕНА\n");
 
+            // запрашиваем у пользователя размерность массива:
+            int AskUserForDimensionOfArray(string msg)
+            {
+                Console.Write(msg);
+                int result = Convert.ToInt32(Console.ReadLine());
+                return result;
+            }
 
+            // создаём двумерный массив рандомных значений:
+            int[,] CreatingTwoDimensionalArrayOfRandomValues(int rows, int columns)
+            {
+                int[,] array = new int[rows, columns];
+                Random rnd = new Random();
+
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        array[i, j] = rnd.Next(1, 10);
+                    }
+                }
+                return array;
+            }
+
+            // вывод рандомного массива в терминал:
+            void OutputOfRandomArrayToTerminal(int[,] array)
+            {
+                Console.WriteLine(" ");
+                for (int i = 0; i < array.GetLength(0); i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        Console.Write("{0,4}", array[i, j]);
+                    }
+                    Console.WriteLine(" ");
+                }
+                Console.WriteLine(" ");
+            }
+
+            // среднее арифметическое элементов по столбцам:
+            void ArithmeticMeanOfElementsByColumns(int[,] arr)
+            {
+                Console.WriteLine("Среднее арифметическое \nкаждого столбца:\n");
+                for (int i = 0; i < arr.GetLength(0); i++)
+                {
+                    double summ = 0;
+                    for (int j = 0; j < arr.GetLength(1); j++)
+                    {
+                        summ += arr[i, j];
+                    }
+                    Console.Write($" {summ / arr.GetLength(0)} ");
+                }
+                Console.WriteLine("\n");
+            }
+
+            int rows = AskUserForDimensionOfArray("Введите кол-во строк: ");
+            int columns = AskUserForDimensionOfArray("Введите кол-во столбцов: ");
+
+            int[,] arr2 = CreatingTwoDimensionalArrayOfRandomValues(rows, columns);
+            OutputOfRandomArrayToTerminal(arr2);
+            ArithmeticMeanOfElementsByColumns(arr2);
+
+            Console.WriteLine("ПРОГРАММА 3 ЗАВЕРШЕНА\n");
             break;
 
         default:
