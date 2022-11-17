eab437ee88dd305619b455b9f049414f9e4b7e69
Klin <piter_frst@yahoo.com>
2022-11-17 21:56:26 +0300

    Task_3 added

--- Home_work_009/Program.cs
+++ Home_work_009/Program.cs
@@ -9,7 +9,7 @@ while (begin)
     Console.WriteLine("Введите номер интересующей вас программы:");
     Console.WriteLine("1 - Программа задаёт значение N и выводит все натуральные числа в промежутке от N до 1.");
     Console.WriteLine("2 - Программа задаёт значения M и N и находит сумму натуральных элементов в промежутке от M до N.");
-    Console.WriteLine("3 - Программа задаёт две матрицы и находит произведение двух матриц.");
+    Console.WriteLine("3 - Программа вычисления функции Аккермана с помощью рекурсии.");
     Console.WriteLine("4 - Если хотите покинуть программу.");
     
     system = Convert.ToInt32(Console.ReadLine());
@@ -102,255 +102,47 @@ while (begin)
 
         case 3:
             /*                                              ///Ветка Task_3.
-            Задача 58: Задайте две матрицы. Напишите программу, 
-            которая будет находить произведение двух матриц.
-            Например, даны 2 матрицы:
-            2 4 | 3 4
-            3 2 | 3 3
-            Результирующая матрица будет:
-            18 20
-            15 18
-            */
+            Задача 68: Напишите программу вычисления функции Аккермана 
+            с помощью рекурсии. Даны два неотрицательных числа m и n.
+            m = 2, n = 3 -> A(m,n) = 9
+            m = 3, n = 2 -> A(m,n) = 29
+            */            
             Console.Clear();
             Console.WriteLine("ПРОГРАММА 3 ЗАПУЩЕНА\n");
 
-            // запрашиваем размерность матриц:
-            int RequestingDimensionOfMatrices(string msg)
+            // ввод значения M и N с консоли:
+            int EnteringTwoValues(string msg)
             {
                 Console.Write(msg);
-                int result = Convert.ToInt32(Console.ReadLine());
+
+                bool isCorrect = false;
+                int result = 0;
+
+                while (!isCorrect)
+                    if (int.TryParse(Console.ReadLine(), out result))
+                        isCorrect = true;
+                    else
+                        Console.WriteLine("Не корректный ввод. Повторите попытку.");
+
                 return result;
             }
 
-            // наполнение матрицы рандомными значениями:
-            int[,] FillArrayRandomNumbers(int[,] matrix)
+            // вывод функции Аккермана:
+            int AckermanFunctions(int m, int n)
             {
-                Random rnd = new Random();
-
-                for (int i = 0; i < matrix.GetLength(0); i++)
-                {
-                    for (int j = 0; j < matrix.GetLength(1); j++)
-                    {
-                        matrix[i, j] = rnd.Next(1, 10);
-                    }
-                }
-                return matrix;
+                if (m == 0) return n + 1;
+                else if (n == 0 && m > 0) return AckermanFunctions(m - 1, 1);
+                else return (AckermanFunctions(m - 1, AckermanFunctions(m, n - 1)));
             }
 
-            // вывод матрицы в консоль:
-            void PrintMatrixInToConsole(int[,] matrix)
-            {
-                for (int i = 0; i < matrix.GetLength(0); i++)
-                {
-                    for (int j = 0; j < matrix.GetLength(1); j++)
-                    {
-                        Console.Write("{0, 4}", matrix[i, j]);
-                    }
-                    Console.WriteLine();
-                }
-            }
-
-            // находим произведение двух матриц:
-            int[,] ProductOfTwoMatrices(int size, int[,] matrixA, int[,] matrixB, int[,] matrixC)
-            {
-                for (int i = 0; i < size; i++)
-                {
-                    for (int j = 0; j < size; j++)
-                    {
-                        for (int k = 0; k < size; k++)
-                        {
-                            matrixC[i, j] = matrixC[i, j] + (matrixA[i, k] * matrixB[k, j]);
-                        }
-                    }
-                }
-                return matrixC;
-            }
-
-            int size = RequestingDimensionOfMatrices("Введите размерность матриц: ");
-            int[,] matrixA = new int[size, size];
-            int[,] matrixB = new int[size, size];
-            int[,] matrixC = new int[size, size];
-
-            FillArrayRandomNumbers(matrixA);
-            FillArrayRandomNumbers(matrixB);
-
-            Console.WriteLine("Первая матрица:");
-            PrintMatrixInToConsole(matrixA);
-            Console.WriteLine();
-            Console.WriteLine("Вторая матрица:");
-            PrintMatrixInToConsole(matrixB);
-            Console.WriteLine();
-
-            int[,] result = ProductOfTwoMatrices(size, matrixA, matrixB, matrixC);
-            Console.WriteLine("Результат произведения двух матриц:");
-            PrintMatrixInToConsole(result);
+            int m = EnteringTwoValues("Введите значение M: ");
+            int n = EnteringTwoValues("Введите значение N: ");
+            Console.WriteLine($"A(m,n) = {AckermanFunctions(m, n)}");
 
             Console.WriteLine("\nПРОГРАММА 3 ЗАВЕРШЕНА\n");
             break;
 
-        case 4:
-            /*                                              ///Ветка Task_4.
-            Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
-            Напишите программу, которая будет построчно выводить массив, 
-            добавляя индексы каждого элемента.
-            Массив размером 2 x 2 x 2
-            66(0,0,0) 25(0,1,0)
-            34(1,0,0) 41(1,1,0)
-            27(0,0,1) 90(0,1,1)
-            26(1,0,1) 55(1,1,1)
-            */
-            Console.Clear();
-            Console.WriteLine("ПРОГРАММА 4 ЗАПУЩЕНА\n");
-
-            // запрашиваем размерность массива:
-            int EnterArraySize(string msg)
-            {
-                Console.Write(msg);
-                int result = int.Parse(Console.ReadLine() ?? "");
-                return result;
-            }
-
-            // заполняем массив рандомными числами:
-            void Fill3DArray(int[,,] array)
-            {
-                int minNum = 10;
-                int maxNum = 99;
-                Random rnd = new Random();
-
-                for (int i = 0; i < array.GetLength(0); i++)
-                {
-                    for (int j = 0; j < array.GetLength(1); j++)
-                    {
-                        for (int k = 0; k < array.GetLength(2); k++)
-                        {
-                            while (array[i, j, k] == 0)
-                            {
-                                int number = rnd.Next(minNum, maxNum + 1);
-
-                                if (IsNumberInArray(array, number) == false)
-                                {
-                                    array[i, j, k] = number;
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-
-            // проверка на повторяемость:
-            bool IsNumberInArray(int[,,] array, int number)
-            {
-                for (int i = 0; i < array.GetLength(0); i++)
-                {
-                    for (int j = 0; j < array.GetLength(1); j++)
-                    {
-                        for (int k = 0; k < array.GetLength(2); k++)
-                        {
-                            if (array[i, j, k] == number) return true;
-                        }
-                    }
-                }
-                return false;
-            }
-
-            // вывод массива в консоль:
-            void Print3DArray(int[,,] array)
-            {
-                for (int i = 0; i < array.GetLength(0); i++)
-                {
-                    for (int j = 0; j < array.GetLength(1); j++)
-                    {
-                        for (int k = 0; k < array.GetLength(2); k++)
-                        {
-                            Console.Write(array[i, j, k]);
-                            Console.Write("({0},{1},{2})\t", i, j, k);
-                        }
-                        Console.WriteLine();
-                    }
-                    Console.WriteLine();
-                }
-            }
-
-            Console.WriteLine($"Введите размер массива A x B x C: ");
-            int sizeA = EnterArraySize("Введите размерность A: ");
-            int sizeB = EnterArraySize("Введите размерность B: ");
-            int sizeC = EnterArraySize("Введите размерность C: ");
-            Console.WriteLine("");
-
-            int[,,] array = new int[sizeA, sizeB, sizeC];
-            Fill3DArray(array);
-            Print3DArray(array);
-
-            Console.WriteLine("\nПРОГРАММА 4 ЗАВЕРШЕНА\n");
-            break;
-
-        case 5:
-            /*                                              ///Ветка Task_5.
-            Задача 62. Напишите программу, которая заполнит спирально массив 4 на 4.
-            Например, на выходе получается вот такой массив:
-            01 02 03 04
-            12 13 14 05
-            11 16 15 06
-            10 09 08 07
-            */
-            Console.Clear();
-            Console.WriteLine("ПРОГРАММА 5 ЗАПУЩЕНА\n");
-
-            // запрашиваем размерность массива:
-            int GetArraySize(string msg)
-            {
-                Console.Write(msg);
-                int size = Convert.ToInt32(Console.ReadLine());
-                return size;
-            }
-
-            // создаём спиральный массив:
-            int[,] CreatingSpiralArray(int size)
-            {
-                int[,] array = new int[size, size];
-
-                int num = 1;
-                int i = 0;
-                int j = 0;
-
-                while (num <= size * size)
-                {
-                    array[i, j] = num;
-                    if (i <= j + 1 && i + j < size - 1)
-                        ++j;
-                    else if (i < j && i + j >= size - 1)
-                        ++i;
-                    else if (i >= j && i + j > size - 1)
-                        --j;
-                    else
-                        --i;
-                    ++num;
-                }
-                return array;
-            }
-
-            void PrintSpiralArray(int[,] array)
-            {
-                for (int i = 0; i < array.GetLength(0); i++)
-                {
-                    for (int j = 0; j < array.GetLength(1); j++)
-                    {
-                        Console.Write("{0, 4}", array[i, j]);
-                    }
-                    Console.WriteLine("");
-                }
-            }
-
-            int sizeX = GetArraySize("Введите размерность массива: ");
-            Console.WriteLine("");
-
-            int[,] arr = CreatingSpiralArray(sizeX);
-            PrintSpiralArray(arr);
-
-            Console.WriteLine("\nПРОГРАММА 5 ЗАВЕРШЕНА\n");
-            break;
-
-        default:
+            default:
             begin = false;
             break;
     }
